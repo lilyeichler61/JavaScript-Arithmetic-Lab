ca9499fdbd8a6ecc62147c8a348071aba8350bcd
Lily Eichler <lilyeichler61@gmail.com>
2020-10-18 22:08:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var num1=parseInt(10,10);
-var num2=parseInt(5,10);
+
 
 function add() {
+  var num1=parseInt(7,10);
+  var num2=parseInt(4,10);
   return (num1+num2);
 }
 function subtract() {
-  return (num1-num2);
+  var num3=parseInt(10,10);
+  var num4=parseInt(5,10);
+  return (num3-num4);
 }
 function multiply(){
-  return (num1*mum2);
+  var num5=parseInt(3, 10);
+  var num6=parseInt(6,10);
+  return (num5*num6);
 }
 function divide() {
-  return (num1/num2);
+  var num7=parseInt(8,10);
+  var num8=parseInt(2,10);
+  return (num7/num8);
 }
