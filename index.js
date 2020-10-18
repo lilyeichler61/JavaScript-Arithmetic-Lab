4babb11f1fbe045102529881e9c9d8ec7ea5c546
Lily Eichler <lilyeichler61@gmail.com>
2020-10-18 22:04:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+num1=10;
+num2=5;
+
+function add() {
+  return (num1+num2);
+}
+function subtract() {
+  return (num1-num2);
+}
+function multiply(){
+  return (num1*mum2);
+}
+function divide() {
+  return (num1/num2);
+}
